8163e18d5af2d25241a6aa5693220706b5fc6f1d
Kori Jackson <korijackson@Mac.localdomain>
2025-10-07 17:40:55 -0400

    Implement phone number standardization

--- Assignment6/Assignment6.py
+++ Assignment6/Assignment6.py
@@ -9,18 +9,28 @@ while True:
     else:
         contact_list.append(line.split("|"))
 
-#print(contact_list)
-
-#clean names
+#clean names and addresses
 for contact in contact_list:
+    #format names
     name_list = contact[0].split()
     formatted_name = " ".join(name_list).title()
     contact[0] = formatted_name
 
+    #format address
     contact[3] = contact[3].strip().title()
     address_list = contact[3].split()
     address_list[-2] = address_list[-2].upper()
     formatted_address = " ".join(address_list)
     contact[3] = formatted_address
 
+    #standardize phone numbers
+    for char in contact[1]:
+        if not char.isdigit():
+            contact[1] = contact[1].replace(char, "")
+    contact[1] = f"({contact[1][0:3]}) {contact[1][3:6]}-{contact[1][6:]}"
+
+
 print(contact_list)
+
+
+
