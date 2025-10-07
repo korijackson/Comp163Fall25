18a61a389aba387419a39caed0e80ae4b3fd2896
Kori Jackson <korijackson@Mac.localdomain>
2025-10-07 17:07:32 -0400

    Add name and address cleaning with string methods

--- Assignment6/Assignment6.py
+++ Assignment6/Assignment6.py
@@ -1,5 +1,4 @@
 #gather input from user; loop until input = DONE
-
 print("Enter contact information (format: name|phone|email|address):" )
 contact_list = []
 
@@ -9,5 +8,19 @@ while True:
         break
     else:
         contact_list.append(line.split("|"))
+
+#print(contact_list)
+
+#clean names
+for contact in contact_list:
+    name_list = contact[0].split()
+    formatted_name = " ".join(name_list).title()
+    contact[0] = formatted_name
+
+    contact[3] = contact[3].strip().title()
+    address_list = contact[3].split()
+    address_list[-2] = address_list[-2].upper()
+    formatted_address = " ".join(address_list)
+    contact[3] = formatted_address
 
 print(contact_list)
